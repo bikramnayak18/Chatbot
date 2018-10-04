89e7a8033bf808c3b2c57ab22c1939ce0d1b3677
bikram nayak <bikram.nayak18@gmail.com>
2018-10-04 22:44:06 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+const Readline = require('readline');
+const rl = Readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
+const matcher = require('./matcher');
+
+
+
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', reply => {
+	matcher(reply, data => {
+		switch(data.intent) {
+			case 'Hello':
+				console.log(`Hi to you too!`);
+				rl.prompt();
+				break;
+			case 'Exit':
+				console.log("Have a great day!");
+				process.exit(0);
+				break;
+
+			default: {
+				console.log("I don't know what you mean :(");
+				rl.prompt();
+			}
+		}
+	});
+});
